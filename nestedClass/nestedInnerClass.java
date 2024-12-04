e39b049f0179e2c401a431dacc7647a0e4bc550a
MH-Rahid <mhrahid112@gmail.com>
2024-12-04 14:20:42 +0800

    nestedclass file added

---
+++ nestedClass/nestedInnerClass.java
@@ -0,0 +1,26 @@
+class OuterClass {
+    private String message = "Hello form outer class ";
+
+    class InnerClass {
+        void display() {
+            System.out.println(message);
+        }
+
+    }
+
+    InnerClass inner = new InnerClass();
+}
+
+public class nestedInnerClass {
+    public static void main(String[] args) {
+        // OuterClass outer = new OuterClass();
+        // OuterClass.InnerClass inner = outer.new InnerClass();
+        // inner.display();
+
+        // OuterClass.InnerClass in = new OuterClass().new InnerClass();
+        // in.display();
+
+        // OuterClass outer = new OuterClass();
+        // outer.inner.display();
+    }
+}
